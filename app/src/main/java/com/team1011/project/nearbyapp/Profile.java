9d3d3ec7ebdd82e49fd6a39b9bac6888a1913b2c
filip <fgutica@gmail.com>
2014-10-20 14:57:07 -0700

    more profile stuff

--- app/src/main/java/com/team1011/project/nearbyapp/Profile.java
+++ app/src/main/java/com/team1011/project/nearbyapp/Profile.java
@@ -1,5 +1,18 @@
 package com.team1011.project.nearbyapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+
 /**
  * Created by Filip on 2014-10-20.
  */
@@ -10,6 +23,11 @@ public class Profile extends PlaceholderFragment {
     private String birthDay;
     private String imageUrl;
     private String aboutMe;
+    private ImageView imgProfilePic;
+    private TextView txtAccountName;
+    private TextView txtDisplayName;
+    private TextView txtBday;
+    private static boolean imageLock = false;
 
    public Profile() {
 
@@ -23,6 +41,59 @@ public class Profile extends PlaceholderFragment {
         birthDay = bDay;
         imageUrl = imgUrl;
         aboutMe = abtMe;
+
+
+
+
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.profile, container, false);
+
+        txtAccountName = (TextView) rootView.findViewById(R.id.accountName);
+        txtDisplayName = (TextView) rootView.findViewById(R.id.displayName);
+        txtBday = (TextView) rootView.findViewById(R.id.birthDay);
+        imgProfilePic = (ImageView) rootView.findViewById(R.id.profilePic);
+
+        if (!imageLock) {
+            //imageLock = true;
+            new LoadProfileImage(imgProfilePic).execute(imageUrl);
+        }
+
+        txtAccountName.setText(userName);
+        txtDisplayName.setText(displayName);
+        txtBday.setText(birthDay);
+
+        return rootView;
+    }
+
+    /**
+     * Background Async task to load user profile picture from url
+     * */
+    private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public LoadProfileImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
