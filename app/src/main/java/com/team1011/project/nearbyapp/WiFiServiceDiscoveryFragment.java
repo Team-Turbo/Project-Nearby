e37750105e47f725e2f97593e00c951328bd2a03
filip <fgutica@gmail.com>
2014-11-01 18:07:14 -0700

    wifi

---
+++ app/src/main/java/com/team1011/project/nearbyapp/WiFiServiceDiscoveryFragment.java
@@ -0,0 +1,355 @@
+package com.team1011.project.nearbyapp;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link WiFiServiceDiscoveryFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link WiFiServiceDiscoveryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class WiFiServiceDiscoveryFragment extends Fragment implements
+        WifiP2pManager.ConnectionInfoListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public static final String TAG = "wifidirectdemo";
+
+    // TXT RECORD properties
+    public static final String TXTRECORD_PROP_AVAILABLE = "available";
+    public static final String SERVICE_INSTANCE = "_wifidemotest";
+    public static final String SERVICE_REG_TYPE = "_presence._tcp";
+
+    public static final int MESSAGE_READ = 0x400 + 1;
+    public static final int MY_HANDLE = 0x400 + 2;
+    private WifiP2pManager manager;
+
+    static final int SERVER_PORT = 4546;
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel channel;
+    private BroadcastReceiver receiver = null;
+    private WifiP2pDnsSdServiceRequest serviceRequest;
+
+    private WiFiDirectServicesList servicesList;
+
+    private TextView statusTxtView;
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment WiFiServiceDiscoveryFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WiFiServiceDiscoveryFragment newInstance(String param1, String param2) {
+        WiFiServiceDiscoveryFragment fragment = new WiFiServiceDiscoveryFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public WiFiServiceDiscoveryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        servicesList = new WiFiDirectServicesList();
+        getFragmentManager().beginTransaction()
+                .add(R.id.container_root, servicesList, "services").commit();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Fragment frag = getActivity().getSupportFragmentManager().findFragmentByTag("services");
+        if (frag != null) {
+            getActivity().getSupportFragmentManager().beginTransaction().remove(frag).commit();
+        }
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_wi_fi_service_discovery, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        statusTxtView = (TextView) getActivity().findViewById(R.id.status_text);
+
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter
+                .addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter
+                .addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager = (WifiP2pManager) getActivity().getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(getActivity(), getActivity().getMainLooper(), null);
+        startRegistrationAndDiscovery();
+
+        servicesList = new WiFiDirectServicesList();
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (manager != null && channel != null) {
+            manager.removeGroup(channel, new WifiP2pManager.ActionListener() {
+
+                @Override
+                public void onFailure(int reasonCode) {
+                    Log.d(TAG, "Disconnect failed. Reason :" + reasonCode);
+                }
+
+                @Override
+                public void onSuccess() {
+                }
+
+            });
+        }
+        super.onDestroy();
+    }
+
+    /**
+     * Registers a local service and then initiates a service discovery
+     */
+    private void startRegistrationAndDiscovery() {
+        Map<String, String> record = new HashMap<String, String>();
+        record.put(TXTRECORD_PROP_AVAILABLE, "visible");
+        record.put("listenport", String.valueOf(SERVER_PORT));
+
+        WifiP2pDnsSdServiceInfo service = WifiP2pDnsSdServiceInfo.newInstance(
+                SERVICE_INSTANCE, SERVICE_REG_TYPE, record);
+        manager.addLocalService(channel, service, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                appendStatus("Added Local Service");
+            }
+
+            @Override
+            public void onFailure(int error) {
+                appendStatus("Failed to add a service");
+            }
+        });
+
+        discoverService();
+
+    }
+
+    private void discoverService() {
+
+        /*
+         * Register listeners for DNS-SD services. These are callbacks invoked
+         * by the system when a service is actually discovered.
+         */
+
+        manager.setDnsSdResponseListeners(channel,
+                new WifiP2pManager.DnsSdServiceResponseListener() {
+
+                    @Override
+                    public void onDnsSdServiceAvailable(String instanceName,
+                                                        String registrationType, WifiP2pDevice srcDevice) {
+
+                        // A service has been discovered. Is this our app?
+
+                        if (instanceName.equalsIgnoreCase(SERVICE_INSTANCE)) {
+
+                            // update the UI and add the item the discovered
+                            // device.
+                            WiFiDirectServicesList fragment = (WiFiDirectServicesList) getActivity().getSupportFragmentManager()
+                                    .findFragmentByTag("services");
+
+                            if (fragment != null) {
+
+                                WiFiDirectServicesList.WiFiDevicesAdapter adapter = ((WiFiDirectServicesList.WiFiDevicesAdapter) fragment
+                                        .getListAdapter());
+                                WiFiP2pService service = new WiFiP2pService();
+                                service.device = srcDevice;
+                                service.instanceName = UI_Shell.userName;
+                                service.serviceRegistrationType = registrationType;
+                                adapter.add(service);
+                                adapter.notifyDataSetChanged();
+                                Log.d(TAG, "onBonjourServiceAvailable "
+                                        + instanceName);
+                            }
+                            else {
+                                Log.d(TAG, "fragment is null");
+                            }
+                        }
+
+                    }
+                }, new WifiP2pManager.DnsSdTxtRecordListener() {
+
+                    /**
+                     * A new TXT record is available. Pick up the advertised
+                     * buddy name.
+                     */
+                    @Override
+                    public void onDnsSdTxtRecordAvailable(
+                            String fullDomainName, Map<String, String> record,
+                            WifiP2pDevice device) {
+                        Log.d(TAG,
+                                device.deviceName + " is "
+                                        + record.get(TXTRECORD_PROP_AVAILABLE));
+                    }
+                });
+
+        // After attaching listeners, create a service request and initiate
+        // discovery.
+        serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+        manager.addServiceRequest(channel, serviceRequest,
+                new WifiP2pManager.ActionListener() {
+
+                    @Override
+                    public void onSuccess() {
+                        appendStatus("Added service discovery request");
+                    }
+
+                    @Override
+                    public void onFailure(int arg0) {
+                        appendStatus("Failed adding service discovery request");
+                    }
+                });
+        manager.discoverServices(channel, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                appendStatus("Service discovery initiated");
+            }
+
+            @Override
+            public void onFailure(int arg0) {
+                appendStatus("Service discovery failed");
+
+            }
+        });
+    }
+
+
+    public void connectP2p(WiFiP2pService service) {
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = service.device.deviceAddress;
+        config.wps.setup = WpsInfo.PBC;
+        if (serviceRequest != null)
+            manager.removeServiceRequest(channel, serviceRequest,
+                    new WifiP2pManager.ActionListener() {
+
+                        @Override
+                        public void onSuccess() {
+                        }
+
+                        @Override
+                        public void onFailure(int arg0) {
+                        }
+                    });
+
+        manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                appendStatus("Connecting to service");
+            }
+
+            @Override
+            public void onFailure(int errorCode) {
+                appendStatus("Failed connecting to service");
+            }
+        });
+    }
+
+
+
+
+
+    @Override
+    public void onConnectionInfoAvailable(WifiP2pInfo info) {
+
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+    public void appendStatus(String status) {
+        String current = statusTxtView.getText().toString();
+        statusTxtView.setText(current + "\n" + status);
+    }
+
+}
