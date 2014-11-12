6085ee5558e4d749d8ad723ffca6ea436dc78c44
filip <fgutica@gmail.com>
2014-11-11 19:40:50 -0800

    asdf

--- app/src/main/java/com/team1011/project/nearbyapp/GCMObject.java
+++ app/src/main/java/com/team1011/project/nearbyapp/GCMObject.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 public class GCMObject {
 
     GoogleCloudMessaging gcm;
-    public static final String SEND_ID = "291560809451";
+    public static final String SEND_ID = "919944179472";
     String registrationID = "";
 
     //GcmBroadcastReceiver incoming;
@@ -88,7 +88,7 @@ public class GCMObject {
 
                 try {
                     httpPost.addHeader(new BasicHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8 "));
-                    httpPost.addHeader(new BasicHeader("Authorization", "key=AIzaSyCJsDphSiFKz42BWOC580RoMEpQ0AeIdkU"));
+                    httpPost.addHeader(new BasicHeader("Authorization", "key=AIzaSyCm2BOndmO5APiW2ywuO1jEbA4pnIxOWTg"));
 
                     String outGoing = "data.text=" + data + "&registration_id=" + regId;
                     StringEntity se = new StringEntity(outGoing);
