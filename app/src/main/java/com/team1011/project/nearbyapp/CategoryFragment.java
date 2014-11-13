9320580873b685b06acae7cff5c88b93e33d3c09
Melvin Loho <mloho@outlook.com>
2014-11-12 22:42:39 -0800

    My categories stuff from today.

--- app/src/main/java/com/team1011/project/nearbyapp/CategoryFragment.java
+++ app/src/main/java/com/team1011/project/nearbyapp/CategoryFragment.java
@@ -6,15 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * Created by Melvin on 10/22/2014.
  *
  * The category fragment.
  */
-public class CategoryFragment extends Fragment {
+public abstract class CategoryFragment extends Fragment {
+
+    enum textType {
+        RECEIVED, SENT
+    }
+
     public static final String ARG_SECTION_NUMBER = "section_number";
+
+    private View mRootView;
+
+    private Category mCategory;
 
     public CategoryFragment(int sectionNumber) {
         Bundle args = new Bundle();
@@ -27,7 +41,8 @@ public class CategoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_placeholder, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_category, container, false);
+        mRootView = rootView;
 
         rootView.findViewById(R.id.placeholder_bg).setBackgroundColor(
                 Color.rgb(
@@ -39,9 +54,48 @@ public class CategoryFragment extends Fragment {
 
         ((TextView) rootView.findViewById(R.id.section_label)).setText(
                 "Category: " + UI_Shell.getCurrentCategory()
-                + "\n" + "Section " + String.valueOf(getArguments().getInt(ARG_SECTION_NUMBER))
+                        + "\n" + "Section " + String.valueOf(getArguments().getInt(ARG_SECTION_NUMBER))
         );
+
+        EditText et;
+
+        Iterator it = mCategory.getElements().entrySet().iterator();
+        while (it.hasNext())
+        {
+            Map.Entry<String, LinkedList<String>> pairs = (Map.Entry<String, LinkedList<String>>)it.next();
+
+            String typeOfElement = pairs.getValue().getFirst();
+
+            if (typeOfElement.equals("edittext")) {
+                processEditText(pairs.getKey());
+            } else {
+                processList(pairs.getKey(), pairs.getValue());
+            }
+        }
 
         return rootView;
     }
+
+    public void setCategory(Category c) {
+        mCategory = c;
+    }
+
+    public abstract String getNotificationText(textType tt);
+
+    public abstract String getTabText(textType tt);
+
+    public abstract Map<String, String> getElements();
+
+    public abstract void setValue(String key, String value);
+
+    /* UI elements with no choices */
+    private void processEditText(String key) {
+
+    }
+
+    /* UI elements with choices, ex. for lists */
+    private void processList(String key, LinkedList<String> choices) {
+
+    }
+
 }
--- app/src/main/java/com/team1011/project/nearbyapp/Category_Shell.java
+++ app/src/main/java/com/team1011/project/nearbyapp/Category_Shell.java
@@ -32,7 +32,7 @@ public class Category_Shell extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        return inflater.inflate(R.layout.fragment_category_shell, container, false);
     }
 
     @Override
